a3a2783fb1f6fe412fa0e4cd0c354456b6405ffe
verykenny <16845292+verykenny@users.noreply.github.com>
2021-10-30 13:35:41 -0600

    modified styles and responsive sizing on the about-team component

--- src/components/about-team.tsx
+++ src/components/about-team.tsx
@@ -61,26 +61,26 @@ const people = [
 function AboutTeam() {
   return (
 <div className="bg-white">
-      <div className="max-w-7xl mx-auto py-12 px-4 text-center sm:px-6 lg:px-8 lg:py-24">
+      <div className="max-w-7xl mx-auto py-12 px-4 text-center sm:px-6 lg:px-8 lg:py-24 bg-violet xl:rounded">
         <div className="space-y-8 sm:space-y-12">
-          <div className="space-y-5 sm:mx-auto sm:max-w-xl sm:space-y-4 lg:max-w-5xl">
-            <h2 className="text-3xl font-extrabold tracking-tight sm:text-4xl">The People</h2>
-            <p className="text-xl text-gray-500">
-            This project would not have been possible without a dedicated team willing to take on the challenge. The countless hours volunteered are donated knowing that this revolutionary resource will save the lives of our community. Our hope is that this site will continue to grow and improve the lives of our LGBTQ+ peers, every person on this list showcased their desire for a more equitable future in healthcare through their commitment. 
+          <div className="space-y-5 sm:mx-auto sm:max-w-xl sm:space-y-4 lg:max-w-5xl text-white">
+            <h2 className="text-3xl font-semibold tracking-wider sm:text-4xl">The People</h2>
+            <p className="text-xl font-light">
+            This project would not have been possible without a dedicated team willing to take on the challenge. The countless hours volunteered are donated knowing that this revolutionary resource will save the lives of our community. Our hope is that this site will continue to grow and improve the lives of our LGBTQ+ peers, every person on this list showcased their desire for a more equitable future in healthcare through their commitment.
             </p>
           </div>
           <ul
             role="list"
-            className="mx-auto grid grid-cols-2 gap-x-4 gap-y-8 sm:grid-cols-4 md:gap-x-6 lg:max-w-5xl lg:gap-x-8 lg:gap-y-12 xl:grid-cols-6"
+            className="mx-auto grid grid-cols-2 gap-x-4 gap-y-8 sm:grid-cols-3 md:grid-cols-4 md:gap-x-6 lg:max-w-5xl lg:gap-x-8 lg:gap-y-12 xl:grid-cols-5"
           >
             {people.map((person) => (
-              <li key={person.name}>
-                <div className="space-y-4">
+              <li key={person.name} className="col-span-1 flex flex-col text-center bg-white rounded-lg shadow divide-y divide-gray-200">
+                <div className="space-y-4 p-3">
                   <img className="mx-auto h-20 w-20 rounded-full lg:w-24 lg:h-24" src={person.imageUrl} alt="" />
                   <div className="space-y-2">
-                    <div className="text-xs font-medium lg:text-sm">
+                    <div className="text-xs font-normal lg:text-sm">
                       <h3>{person.name}</h3>
-                      <p className="text-indigo-600">{person.role}</p>
+                      <p className="text-gray-800 font-semibold">{person.role}</p>
                     </div>
                   </div>
                 </div>
