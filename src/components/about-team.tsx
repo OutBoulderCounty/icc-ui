c6d8c7ef4bdf997d72291596354c9755fbbb0deb
verykenny <16845292+verykenny@users.noreply.github.com>
2021-10-30 11:24:52 -0600

    replaced template

--- src/components/about-team.tsx
+++ src/components/about-team.tsx
@@ -12,31 +12,34 @@ const people = [
 
 function AboutTeam() {
   return (
-    <div className="bg-white">
-      <div className="mx-auto py-12 px-4 max-w-7xl sm:px-6 lg:px-8 lg:py-24">
-        <div className="grid grid-cols-1 gap-12 lg:grid-cols-3 lg:gap-8">
-          <div className="space-y-5 sm:space-y-4">
-            <h2 className="text-3xl font-extrabold tracking-tight sm:text-4xl">Meet our leadership</h2>
+<div className="bg-white">
+      <div className="max-w-7xl mx-auto py-12 px-4 text-center sm:px-6 lg:px-8 lg:py-24">
+        <div className="space-y-8 sm:space-y-12">
+          <div className="space-y-5 sm:mx-auto sm:max-w-xl sm:space-y-4 lg:max-w-5xl">
+            <h2 className="text-3xl font-extrabold tracking-tight sm:text-4xl">The People</h2>
             <p className="text-xl text-gray-500">
-              Libero fames augue nisl porttitor nisi, quis. Id ac elit odio vitae elementum enim vitae ullamcorper
-              suspendisse. Vivamus fringilla.
+              Risus velit condimentum vitae tincidunt tincidunt. Mauris ridiculus fusce amet urna nunc. Ut nisl ornare
+              diam in.
             </p>
           </div>
-          <div className="lg:col-span-2">
-            <ul role="list" className="space-y-12 sm:grid sm:grid-cols-2 sm:gap-12 sm:space-y-0 lg:gap-x-8">
-              {people.map((person) => (
-                <li key={person.name}>
-                  <div className="flex items-center space-x-4 lg:space-x-6">
-                    <img className="w-16 h-16 rounded-full lg:w-20 lg:h-20" src={person.imageUrl} alt="" />
-                    <div className="font-medium text-lg leading-6 space-y-1">
+          <ul
+            role="list"
+            className="mx-auto grid grid-cols-2 gap-x-4 gap-y-8 sm:grid-cols-4 md:gap-x-6 lg:max-w-5xl lg:gap-x-8 lg:gap-y-12 xl:grid-cols-6"
+          >
+            {people.map((person) => (
+              <li key={person.name}>
+                <div className="space-y-4">
+                  <img className="mx-auto h-20 w-20 rounded-full lg:w-24 lg:h-24" src={person.imageUrl} alt="" />
+                  <div className="space-y-2">
+                    <div className="text-xs font-medium lg:text-sm">
                       <h3>{person.name}</h3>
                       <p className="text-indigo-600">{person.role}</p>
                     </div>
                   </div>
-                </li>
-              ))}
-            </ul>
-          </div>
+                </div>
+              </li>
+            ))}
+          </ul>
         </div>
       </div>
     </div>
