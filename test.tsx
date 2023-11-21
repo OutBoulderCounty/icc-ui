0aa0a9ebd16e7ae26e0d21ec4d08b3df3617ef43
Houda Jawad <houdajawad@gmail.com>
2023-11-20 20:19:52 -0800

    Add first tests for 404 page and download accessibility extention for jest

--- test.tsx
+++ test.tsx
@@ -1,8 +1,26 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import NotFoundPage from './src/pages/404'; // Adjust the path based on your project structure
+import NotFoundPage from './src/pages/404';
+import { axe } from 'jest-axe';
+import 'jest-axe/extend-expect';
 
-test('renders the NotFoundPage with the correct title', () => {
-  render(<NotFoundPage />);
-  
+describe('NotFoundPage Component', () => {
+  it('renders without crashing', () => {
+    render(<NotFoundPage />);
+  });
+
+  it('renders the correct content', () => {
+    render(<NotFoundPage />);
+
+    expect(screen.getByText('404: Not Found')).toBeInTheDocument();
+    expect(
+      screen.getByText("You just hit a route that doesn't exist... the sadness.")
+    ).toBeInTheDocument();
+  });
+
+  it('passes accessibility tests', async () => {
+    const { container } = render(<NotFoundPage />);
+    const results = await axe(container);
+    expect(results).toHaveNoViolations();
+  });
 });
